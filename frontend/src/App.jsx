f8e6c0c585e1fd0810fe5d8282cc13589f96ef7f
angiedejauregui <angiedejauregui@gmail.com>
2025-06-16 16:51:35 -0300

    add protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,34 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Welcome from './pages/Welcome'
-import ForgotPassword from './pages/ForgotPassword'
-import ResetPassword from './pages/ResetPassword'
-import Logo from './pages/Logo'
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Welcome from "./pages/Welcome";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Logo from "./pages/Logo";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <div>
-      <Logo />
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/welcome" element={<Welcome />} />
-        <Route path='/forgot-password' element={<ForgotPassword />} />
-        <Route path='/reset-password/:token' element={<ResetPassword />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
-  )
+      <div>
+        <Logo />
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/welcome"
+            element={
+              <PrivateRoute>
+                <Welcome />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
