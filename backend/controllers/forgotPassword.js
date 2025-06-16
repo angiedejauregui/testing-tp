6a3820e202c989f260f42d28abbc9c98b243747b
angiedejauregui <angiedejauregui@gmail.com>
2025-06-16 15:37:38 -0300

    change resetLink

--- backend/controllers/forgotPassword.js
+++ backend/controllers/forgotPassword.js
@@ -21,9 +21,7 @@ const forgotPassword = async (req, res) => {
       expiresIn: "1h",
     });
 
-    const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"; 
-
-    const resetLink = `${FRONTEND_URL}/reset-password/${token}`;
+    const resetLink = `$https://testing-tp.vercel.app//reset-password/${token}`;
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
